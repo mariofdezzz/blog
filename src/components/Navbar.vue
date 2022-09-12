082e2f0cc7525b4f74a2441b6d3d9c394c78e21d
Mario Ferrero <47794126+mariofdezzz@users.noreply.github.com>
2022-09-12 17:21:34 +0100

    feat: improve navbar look&feel

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,24 +1,36 @@
 <template>
   <header>
-    <h4>
-      ./mariofdezzz.dev
-    </h4>
+    <nav>
+      <div class="navbar-brand">
+        <h4>
+          <NuxtLink to="/">
+            ./mariofdezzz.dev
+          </NuxtLink>
+        </h4>
+      </div>
+    </nav>
     <!-- TODO: Add color theme, add lang selector -->
   </header>
 </template>
 
 <style scoped lang="scss">
 header {
-  margin-top: 1.5rem;
+  padding-top: 1.5rem ;
   display: grid;
   grid-template-columns: 1fr min($max-text-width, calc(100% - 2rem)) 1fr;
   row-gap: 2rem;
   column-gap: 1rem;
 
-  h4 {
+  nav {
     // font-family: Consolas, monospace;
     grid-column: 2;
     letter-spacing: 1px;
+    border-bottom: 1px solid $on-bg-disabled;
+    padding-bottom: 0.5rem;
+
+    h4 > a {
+      color: $on-bg;
+    }
   }
 }
 </style>
