2ba1e7fbee22c5b154dcba4aad2f7ef9c616b136
Mario Ferrero <47794126+mariofdezzz@users.noreply.github.com>
2022-09-12 17:11:21 +0100

    feat: add index page content
    work in progress

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,48 @@
 <template>
-  <div />
+  <div class="no-posts">
+    <h2>
+      Nothing to see here
+      <span class="blinking">
+        yet
+      </span>
+    </h2>
+
+    <img src="assets/images/pixel-doggo.gif" alt="Swimming dog">
+  </div>
 </template>
+
+<style scoped lang="scss">
+.no-posts {
+  text-align: center;
+
+  h2 {
+    margin-bottom: 2rem;
+    opacity: 0.8;
+  }
+
+  img {
+    margin: auto;
+    width: 5rem;
+  }
+}
+
+.blinking {
+  opacity: 0;
+  animation: blinking 10s ease infinite;
+}
+
+@keyframes blinking {
+  30% {
+    opacity: 0;
+  }
+  40% {
+    opacity: 1;
+  }
+  60% {
+    opacity: 1;
+  }
+  70% {
+    opacity: 0;
+  }
+}
+</style>
